6967f6881ce736056ecc8f1c8f042949ccfc0ae3
Nate Hodge <hodge.nate@gmail.com>
2018-04-19 11:28:52 -0400

    Initial framework/engine.

---
+++ Assets/CustomizableEngine/StockScenes/TitleCanvas.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleCanvas : MonoBehaviour 
+{
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+
+	public void OnGoToGame()
+	{
+		GameController.GetController<GameStateEngine>().ChangeGameState(GameStateEngine.States.Playing);
+	}
+
+	//public void OnGoToTerrainSandbox()
+	//{
+	//	GameController.GetController<GameStateEngine>().ChangeGameState(GameStateEngine.States.TerrainSandbox);
+	//}
+
+	//public void OnGoToNavMeshScene()
+	//{
+	//	GameController.GetController<GameStateEngine>().ChangeGameState(GameStateEngine.States.NavMeshSandbox);
+	//}
+
+	//public void OnGoToPlayerSandbox()
+	//{
+	//	GameController.GetController<GameStateEngine>().ChangeGameState(GameStateEngine.States.PlayerSandbox);
+	//}
+}
